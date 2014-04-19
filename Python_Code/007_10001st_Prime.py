0b3070082b5ca72d569247f001e43e93f9b15f46
minqiang <minqiang.li@gmail.com>
2014-04-18 23:35:16 -0400

    added 007***.py

---
+++ Python_Code/007_10001st_Prime.py
@@ -0,0 +1,46 @@
+#===============================================================================
+# Problem 7:
+#
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
+# we can see that the 6th prime is 13.
+#
+# What is the 10001st prime number?
+#===============================================================================
+
+# Author: Minqiang Li (any comments welcome at myfirstname.mylastname@gmail.com!
+# https://github.com/minqiang/ProjectEuler
+
+#===============================================================================
+# Simple method by checking numbers one by one using the function is_divisible. 
+# The primes are saved in the list plist to speed up computation.
+#===============================================================================
+
+from math import sqrt
+
+# given number num, check whether divisible by any number in plist
+def is_divisible(num, plist):
+    m = long(sqrt(num))+1
+    for j in plist:
+        if j>m:
+            return True
+        if num%j == 0:
+            return False
+    return True
+
+plist = [2,3]
+num_prime = 2
+i = plist[-1]
+
+limit = 10001
+while num_prime < limit:
+    i += 2
+    if is_divisible(i, plist):
+        plist.append(i)
+        num_prime += 1
+
+print(plist[-1])
+        
+        
+        
+
+    
