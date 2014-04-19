cc7d2254968da22021c0671a0cb19344e1c85321
minqiang <minqiang.li@gmail.com>
2014-04-19 18:03:41 -0400

    added 010***.py, fixed buy in 007

--- Python_Code/010_summation_of_primes.py
+++ Python_Code/010_summation_of_primes.py
@@ -1,25 +1,26 @@
 #===============================================================================
-# Problem 7:
+# Problem 10: 
 #
-# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
-# we can see that the 6th prime is 13.
-#
-# What is the 10001st prime number?
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# 
+# Find the sum of all the primes below two million.
 #===============================================================================
 
 # Author: Minqiang Li (any comments welcome at myfirstname.mylastname@gmail.com!
 # https://github.com/minqiang/ProjectEuler
 
 #===============================================================================
-# Simple method by checking numbers one by one using the function is_divisible. 
-# The primes are saved in the list plist to speed up computation.
+# This problem is related to Problem 7. 
+# In fact, code change is extremely minimal.
+# Any idea of speeding up code is welcome.
 #===============================================================================
+
 
 from math import sqrt
 
 # given number num, check whether divisible by any number in plist
 def not_divisible(num, plist):
-    m = long(sqrt(num))+1
+    m = long(sqrt(num))
     for j in plist:
         if j>m:
             return True
@@ -28,19 +29,15 @@ def not_divisible(num, plist):
     return True
 
 plist = [2,3]
-num_prime = 2
+sum_prime = 5
 i = plist[-1]
 
-limit = 10001
-while num_prime < limit:
+limit = 2000000
+while i < limit:
     i += 2
     if not_divisible(i, plist):
         plist.append(i)
-        num_prime += 1
+        sum_prime += i
 
-print(plist[-1])
+print(sum_prime)
         
-        
-        
-
-    
