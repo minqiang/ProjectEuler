67ee796024b7fea97d816b83ad6713a0f4b5e998
Minqiang Li <minqiang.li@gmail.com>
2014-04-22 01:31:48 -0400

    simplified 001.py

--- Python_Code/001_Multiples_of_3_and_5.py
+++ Python_Code/001_Multiples_of_3_and_5.py
@@ -12,5 +12,4 @@
 #===============================================================================
 
 multiples = [x for x in range(1000) if (x%3==0 or x%5==0)]
-result = reduce(lambda a,b: a+b, multiples)
-print(result)
+print(sum(multiples))
